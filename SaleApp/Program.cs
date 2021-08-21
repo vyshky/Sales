ca007c9cfaea6a7985b97756ea9c3a33e2ae21a2
Alexander Deyukhin <vyshky2@Gmail.com>
2021-08-21 12:09:28 +0400

    Инициализациия проэкта

---
+++ SaleApp/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SaleApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
